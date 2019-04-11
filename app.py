55c512677b96d84692f53e794ce186ddd7b65564
Austin <austin.yau@gmail.com>
2019-04-11 15:38:25 -0700

    switching to cookie sessions instead

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash, session
 from surveys import satisfaction_survey
 
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# app.secret_key = 'jacqueeee'
 
 debug = DebugToolbarExtension(app)
 
-responses = []
+# responses = []
 questions = satisfaction_survey.questions
 
 
@@ -22,13 +23,30 @@ def start_page():
     return render_template("base.html", title=title, instruction=instruction, questions=questions)
 
 
+@app.route('/set-cookie', methods=["POST"])
+def set_cookie():
+    
+    session["responses"] = []
+    current_question = 0
+
+    return redirect(f"/question/{current_question}")
+
+
 @app.route('/question/<int:question_index>')
 def handle_question(question_index):
-    
-    question = questions[question_index].question
-    choices = questions[question_index].choices
-    question_index = question_index
-    return render_template("question.html", question=question, index=question_index, choices=choices)
+    if len(session["responses"]) == len(questions):
+        return redirect('/thanks')
+
+    current_question_index = len(session["responses"])
+
+    if question_index != current_question_index:
+        flash("Trying to access invalid question!")
+        return redirect(f"/question/{current_question_index}")
+
+    question = questions[current_question_index].question
+    choices = questions[current_question_index].choices
+    # question_index = question_index
+    return render_template("question.html", question=question, index=current_question_index, choices=choices)
 
 
 @app.route('/answer', methods=["POST"])
@@ -36,8 +54,25 @@ def post_answer():
 
     # next_question = request.form['']
     values = request.form
+    responses = session["responses"]
     answer = values[str(len(responses))]
-    next_question = len(responses) + 1
     responses.append(answer)
-
+    session["responses"] = responses
+    next_question = len(responses)
+    
     return redirect(f"/question/{next_question}")
+
+@app.route('/thanks')
+def thanks():
+    return """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Thanks for completing the survey!</h1>
+</body>
+</html>"""

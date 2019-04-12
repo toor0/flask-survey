a220b7c5de3feef81b5c31b5da6c207960186200
Austin <austin.yau@gmail.com>
2019-04-11 21:37:35 -0700

    further study 2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, redirect, flash, session
-from surveys import satisfaction_survey
+from surveys import satisfaction_survey, surveys
 
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -11,16 +11,26 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 # responses = []
-questions = satisfaction_survey.questions
 
 
-@app.route('/start')
+@app.route('/selection')
+def selection_page():
+
+    return render_template("selection.html", surveys=surveys)
+
+
+@app.route('/start', methods=["GET"])
 def start_page():
 
-    title = satisfaction_survey.title
-    instruction = satisfaction_survey.instructions
+    selected_survey = request.args["survey"]
 
-    return render_template("base.html", title=title, instruction=instruction, questions=questions)
+    title = surveys[selected_survey].title
+    instructions = surveys[selected_survey].instructions
+    print(surveys[selected_survey].questions)
+    session["questions"] = selected_survey
+    # session["questions"] = surveys[selected_survey].questions
+
+    return render_template("base.html", title=title, instruction=instructions)
 
 
 @app.route('/set-cookie', methods=["POST"])
@@ -34,17 +44,21 @@ def set_cookie():
 
 @app.route('/question/<int:question_index>')
 def handle_question(question_index):
-    if len(session["responses"]) == len(questions):
-        return redirect('/thanks')
+    # if len(session["responses"]) == len(questions):
+    #     return redirect('/thanks')
 
     current_question_index = len(session["responses"])
 
-    if question_index != current_question_index:
-        flash("Trying to access invalid question!")
-        return redirect(f"/question/{current_question_index}")
+    # if question_index != current_question_index:
+    #     flash("Trying to access invalid question!")
+    #     return redirect(f"/question/{current_question_index}")
 
-    question = questions[current_question_index].question
-    choices = questions[current_question_index].choices
+    survey_id = session["questions"]
+
+    question = surveys[survey_id].questions[current_question_index].question
+    choices = surveys[survey_id].questions[current_question_index].choices
+  
+    # choices = session["questions"][question_index].choices
     # question_index = question_index
     return render_template("question.html", question=question, index=current_question_index, choices=choices)
 
